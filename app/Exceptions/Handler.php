d2ce200ae6a032fc084f4349af282afb31dca981
hsynsrts <32297624+hsynsrts@users.noreply.github.com>
2019-10-06 11:05:56 +0300

    İlk Yükleme

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,9 @@
 namespace App\Exceptions;
 
 use Exception;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -46,6 +49,19 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if($exception instanceof NotFoundHttpException){
+            return response()->view('errors.404',compact('exception'),404);
+        }
+        if($exception instanceof ModelNotFoundException){
+            return response()->view('errors.404',compact('exception'),404);
+        }
         return parent::render($request, $exception);
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $request->expectsJson()
+            ? response()->json(['message' => 'Unauthenticated'],401)
+            : redirect()->guest(route('kullanici.oturumac'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,33 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/','AnasayfaController@index')->name('anasayfa');
+
+Route::get('/kategori/{slug_kategoriadi}','KategoriController@index')->name('kategori');
+
+Route::get('/urun/{slug_urunadi}','UrunController@index')->name('urun');
+
+Route::post('/ara','UrunController@ara')->name('urun_ara');
+Route::get('/ara','UrunController@ara')->name('urun_ara');
+
+Route::get('/sepet}','SepetController@index')->name('sepet');
+
+Route::group(['middleware' => 'auth'],function (){
+    Route::get('/odeme}','OdemeController@index')->name('odeme');
+    Route::get('/siparisler}','SiparisController@index')->name('siparisler');
+    Route::get('/siparis/{id}','SiparisController@detay')->name('siparis');
 });
 
-Route::get('/merhaba',function (){
-    return "Merhaba";
+Route::group(['prefix'=>'kullanici'],function (){
+    Route::get('/oturumac','KullaniciController@giris_form')->name('kullanici.oturumac');
+    Route::post('/oturumac','KullaniciController@giris');
+    Route::get('/kaydol','KullaniciController@kaydol_form')->name('kullanici.kaydol');
+    Route::post('/kaydol','KullaniciController@kaydol');
+    Route::get('/aktiflestir/{anahtar}','KullaniciController@aktiflestir')->name('aktiflestir');
+    Route::post('/oturumukapat','KullaniciController@oturumukapat')->name('kullanici.oturumukapat');
 });
 
-Route::get('api/v1/merhaba',function (){
-    return ['mesaj'=>'merhaba'];
+Route::get('/test/mail',function (){
+    $kullanici = App\Models\Kullanici::find(1);
+    return new App\Mail\KullaniciKayitMail();
 });
